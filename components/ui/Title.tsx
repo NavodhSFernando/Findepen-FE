b4446b672cc8eb6a9bb0629060fc540a4fa68f85
Navodh Fernando <Navodh123@outlook.com>
2025-02-23 11:18:52 +0530

    Added title component

--- components/ui/Title.tsx
+++ components/ui/Title.tsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Colors } from "@/constants/Colors";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 interface TitleProps {
-    text: string;
+  text: string;
 }
 
 const Title: React.FC<TitleProps> = ({ text }) => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>{text}</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{text}</Text>
+    </View>
+  );
 };
 
 export default Title;
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        width: '100%',
-    },
-    title: {
-        fontSize: 20,
-        fontFamily: 'JakarthaBold',
-        marginVertical: 40,
-        textAlign: 'center',
-    },
+  container: {
+    display: "flex",
+    width: "100%",
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: "JakarthaBold",
+    paddingTop: 30,
+    textAlign: "center",
+    color: Colors.text,
+  },
 });
